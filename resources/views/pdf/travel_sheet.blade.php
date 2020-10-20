07d6e164825edfef4e832c902df364cc16c2f2d9
Kurt dela Rosa <ak.delarosa@seiko-it.com.ph>
2020-10-20 20:56:27 +0800

    modify travel sheet PDF

--- resources/views/pdf/travel_sheet.blade.php
+++ resources/views/pdf/travel_sheet.blade.php
@@ -22,6 +22,9 @@
         thead{display: table-header-group;}
 		tfoot {display: table-row-group;}
 		tr {page-break-inside: avoid;}
+		.remarks {
+			padding-top: -50px;
+		}
     </style>
     <style type="text/css">
 	    .page
@@ -41,54 +44,71 @@
 	?>
 					<div class="container page">
 						<div class="row">
-							<div class="col-xs-6" style="padding-right: 200px">
+							<div class="col-xs-6" style="padding-right: 20px">
 								<img src="{{ asset($header->iso_photo) }}" alt="" height="80px" width="200px" >
 							</div>
-							<div class="col-xs-6 text-center">
-								<h3>TRAVEL SHEET</h3>
+							<div class="col-xs-5 text-center" style="width: 500px;">
+								<h2 style="padding-top:40px;">TRAVEL SHEET</h2>
+							</div>
+							<div class="col-xs-1" style="width: 217px;">
+								<p style="padding-top:50px; padding-bottom: 0px;font-size: 13px; text-align:right">Date Released: {{ $pages['date'] }}</p>
 							</div>
 							    
 						</div>
 						<div class="row">
-							<div class="col-xs-12">
-								<table class="table table-bordered text-center table-sm" style="font-size:11px;width:100%;">
+							<div class="col-xs-12" style="width:100%;">
+								<table class="table table-bordered table-sm" style="font-size:13px;width:100%;">
 									<thead>
 										<tr>
-											<td rowspan="2" colspan="4"><img src="{{ asset('/barcode.php').'?codetype=CODE39&size=30&text='.$header->jo_sequence.'&print=true' }}" class="img-fluid" alt="{{ $header->jo_sequence }}"></td> 
+											<td rowspan="2" colspan="3"><img src="{{ asset('/barcode.php').'?codetype=CODE39&size=30&text='.$header->jo_sequence.'&print=true' }}" class="img-fluid" alt="{{ $header->jo_sequence }}"></td> 
 											<td colspan="2">Type:</td>
-											<td colspan="2">{{ $header->type }}</td>
-											<td rowspan="2">Material Heat #:</td>
-											<td colspan="2" rowspan="2">{{ $header->material_heat_no }}</td>
-										</tr>
-										<tr>
+											<td colspan="3">{{ $header->type }}</td>
 											<td colspan="2">Material Used:</td>
+											<td>{{ $header->bar_size }}</td>
+											{{-- <td>{{ $header->bar_size.' x '.$header->cut_weight }}</td> --}}
+										</tr>
+										<tr>
+											{{-- <td colspan="2">Material Used:</td>
 											<td>{{ $header->material_used }}</td>
-											<td>{{ $header->bar_size.' x '.$header->cut_weight }}</td>
-										</tr>
-										<tr>
-											<td colspan="4">Production Order Number</td>
-											<td colspan="2">Issued Qty:</td>
-											<td colspan="2">{{ $header->issued_qty }}</td>
-											
-											<td>Part Code:</td>
-											<td colspan="2">{{ $header->prod_code }}</td>
-										</tr>
-										<tr>
-											<td colspan="4">{{ $header->sc_no }}</td>
-											<td colspan="2" rowspan="2">Lot No.</td>
-											<td colspan="2" rowspan="2">{{ $header->prod_heat_no }}</td>
-											<td rowspan="2">Description</td>
-											<td colspan="2" rowspan="2">{{ $header->description }}</td>
+											<td>{{ $header->bar_size.' x '.$header->cut_weight }}</td> --}}
+											<td colspan="2">Part Code:</td>
+											<td colspan="3">{{ $header->prod_code }}</td>
+
+											<td colspan="2">Material Heat #:</td>
+											<td>{{ $header->material_heat_no }}</td>
 										</tr>
 										<tr>
 											<td>Job Order Qty.</td>
-											<!-- $header->order_qty -->
 											<td colspan="2">{{ $header->sched_qty }}</td>
-											<td>PCS.</td>
+											<td colspan="2" rowspan="2">Description</td>  {{-- rowspan="2" --}}
+											<td colspan="3" rowspan="2">{{ $header->description }}</td>{{-- colspan="2" rowspan="2" --}}
+											{{-- <td colspan="2">{{ $header->issued_qty }}</td> --}}
+											
+											<td colspan="2">Supplier Heat #:</td>
+											<td>{{ $header->supplier_heat_no }}</td>
+										</tr>
+										<tr>
+											<td>Product Line</td>
+											<td colspan="2">{{ $header->product_line }}</td>
+											<td>Cut Wt.</td>
+											<td>Cut Length</td>
+											<td>Cut Width</td>
+										</tr>
+										<tr>
+											<td>S/C #</td>
+											<td colspan="2">{{ $header->sc_no }}</td>
+											<td>Issued Qty.</td>
+											<td>{{ $header->issued_qty }}</td>
+											<td>Lot No.</td>
+											<td colspan="2">{{ $header->lot_no }}</td>
+
+											<td>{{ $header->cut_weight }}</td> {{-- cut wt --}}
+											<td>{{ $header->cut_length }}</td> {{-- cut length --}}
+											<td>{{ $header->cut_width}}</td> {{-- cut width --}}
 										</tr>
 										<tr>
 											<td rowspan="2">Process</td>
-											<td rowspan="2">MC/ No.</td>
+											<td rowspan="2">Division No.</td>
 											<td rowspan="2">Date Process</td>
 											<td rowspan="2">Input Qty.</td>
 											<td rowspan="2">Output Qty.</td>
@@ -130,17 +150,19 @@
 									</tbody>
 									<tfoot>
 										<tr>
-											<td>Remarks</td>
-											<td colspan="10"></td>
-										</tr>
-										<tr>
-											<td colspan="8">
+											<td colspan="11">
 												<b>Legend:</b> A - for Rework | B - for Scrap | C - Alloy Mix | NC - Non-Conformance
 											</td>
-											<td colspan="3">
+											{{-- <td colspan="3">
 												Date Released: {{ $pages['date'] }}
+											</td> --}}
+										</tr>
+										<tr>
+											<td colspan="11" style="height: 100px">
+												<strong>REMARKS</strong>
 											</td>
 										</tr>
+										
 									</tfoot>
 								</table>
 								
@@ -148,8 +170,8 @@
 						</div>
 
 						<div class="row">
-							<div class="col-xs-1" style="text-align:right">
-								<span class="small">{{ $header->iso_code }}</span>
+							<div class="col-xs-1" style="width:100%;text-align:right">
+								<strong class="small">ISO Control No.: {{ $header->iso_code }}</strong>
 							</div>
 						</div>
 					</div>
--- storage/framework/views/f665d7105e837a8b00a43b1c3ff296660384c610.php
+++ storage/framework/views/f665d7105e837a8b00a43b1c3ff296660384c610.php
@@ -20,6 +20,9 @@
         thead{display: table-header-group;}
 		tfoot {display: table-row-group;}
 		tr {page-break-inside: avoid;}
+		.remarks {
+			padding-top: -50px;
+		}
     </style>
     <style type="text/css">
 	    .page
@@ -39,54 +42,69 @@
 	?>
 					<div class="container page">
 						<div class="row">
-							<div class="col-xs-6" style="padding-right: 200px">
+							<div class="col-xs-6" style="padding-right: 20px">
 								<img src="<?php echo e(asset($header->iso_photo)); ?>" alt="" height="80px" width="200px" >
 							</div>
-							<div class="col-xs-6 text-center">
-								<h3>TRAVEL SHEET</h3>
+							<div class="col-xs-5 text-center" style="width: 500px;">
+								<h2 style="padding-top:40px;">TRAVEL SHEET</h2>
+							</div>
+							<div class="col-xs-1" style="width: 217px;">
+								<p style="padding-top:50px; padding-bottom: 0px;font-size: 13px; text-align:right">Date Released: <?php echo e($pages['date']); ?></p>
 							</div>
 							    
 						</div>
 						<div class="row">
-							<div class="col-xs-12">
-								<table class="table table-bordered text-center table-sm" style="font-size:11px;width:100%;">
+							<div class="col-xs-12" style="width:100%;">
+								<table class="table table-bordered table-sm" style="font-size:13px;width:100%;">
 									<thead>
 										<tr>
-											<td rowspan="2" colspan="4"><img src="<?php echo e(asset('/barcode.php').'?codetype=CODE39&size=30&text='.$header->jo_sequence.'&print=true'); ?>" class="img-fluid" alt="<?php echo e($header->jo_sequence); ?>"></td> 
+											<td rowspan="2" colspan="3"><img src="<?php echo e(asset('/barcode.php').'?codetype=CODE39&size=30&text='.$header->jo_sequence.'&print=true'); ?>" class="img-fluid" alt="<?php echo e($header->jo_sequence); ?>"></td> 
 											<td colspan="2">Type:</td>
-											<td colspan="2"><?php echo e($header->type); ?></td>
-											<td rowspan="2">Material Heat #:</td>
-											<td colspan="2" rowspan="2"><?php echo e($header->material_heat_no); ?></td>
-										</tr>
-										<tr>
+											<td colspan="3"><?php echo e($header->type); ?></td>
 											<td colspan="2">Material Used:</td>
-											<td><?php echo e($header->material_used); ?></td>
-											<td><?php echo e($header->bar_size.' x '.$header->cut_weight); ?></td>
-										</tr>
-										<tr>
-											<td colspan="4">Production Order Number</td>
-											<td colspan="2">Issued Qty:</td>
-											<td colspan="2"><?php echo e($header->issued_qty); ?></td>
+											<td><?php echo e($header->bar_size); ?></td>
 											
-											<td>Part Code:</td>
-											<td colspan="2"><?php echo e($header->prod_code); ?></td>
 										</tr>
 										<tr>
-											<td colspan="4"><?php echo e($header->sc_no); ?></td>
-											<td colspan="2" rowspan="2">Lot No.</td>
-											<td colspan="2" rowspan="2"><?php echo e($header->prod_heat_no); ?></td>
-											<td rowspan="2">Description</td>
-											<td colspan="2" rowspan="2"><?php echo e($header->description); ?></td>
+											
+											<td colspan="2">Part Code:</td>
+											<td colspan="3"><?php echo e($header->prod_code); ?></td>
+
+											<td colspan="2">Material Heat #:</td>
+											<td><?php echo e($header->material_heat_no); ?></td>
 										</tr>
 										<tr>
 											<td>Job Order Qty.</td>
-											<!-- $header->order_qty -->
 											<td colspan="2"><?php echo e($header->sched_qty); ?></td>
-											<td>PCS.</td>
+											<td colspan="2" rowspan="2">Description</td>  
+											<td colspan="3" rowspan="2"><?php echo e($header->description); ?></td>
+											
+											
+											<td colspan="2">Supplier Heat #:</td>
+											<td><?php echo e($header->supplier_heat_no); ?></td>
+										</tr>
+										<tr>
+											<td>Product Line</td>
+											<td colspan="2"><?php echo e($header->product_line); ?></td>
+											<td>Cut Wt.</td>
+											<td>Cut Length</td>
+											<td>Cut Width</td>
+										</tr>
+										<tr>
+											<td>S/C #</td>
+											<td colspan="2"><?php echo e($header->sc_no); ?></td>
+											<td>Issued Qty.</td>
+											<td><?php echo e($header->issued_qty); ?></td>
+											<td>Lot No.</td>
+											<td colspan="2"><?php echo e($header->lot_no); ?></td>
+
+											<td><?php echo e($header->cut_weight); ?></td> 
+											<td><?php echo e($header->cut_length); ?></td> 
+											<td><?php echo e($header->cut_width); ?></td> 
 										</tr>
 										<tr>
 											<td rowspan="2">Process</td>
-											<td rowspan="2">MC/ No.</td>
+											<td rowspan="2">Division No.</td>
 											<td rowspan="2">Date Process</td>
 											<td rowspan="2">Input Qty.</td>
 											<td rowspan="2">Output Qty.</td>
@@ -128,18 +146,17 @@
 									</tbody>
 									<tfoot>
 										<tr>
-											<td>Remarks</td>
-											<td colspan="10"></td>
-										</tr>
-										<tr>
-											<td colspan="8">
+											<td colspan="11">
 												<b>Legend:</b> A - for Rework | B - for Scrap | C - Alloy Mix | NC - Non-Conformance
 											</td>
-											<td colspan="3">
-												Date Released: <?php echo e($pages['date']); ?>
-
+											
+										</tr>
+										<tr>
+											<td colspan="11" style="height: 100px">
+												<strong>REMARKS</strong>
 											</td>
 										</tr>
+										
 									</tfoot>
 								</table>
 								
@@ -147,8 +164,8 @@
 						</div>
 
 						<div class="row">
-							<div class="col-xs-1" style="text-align:right">
-								<span class="small"><?php echo e($header->iso_code); ?></span>
+							<div class="col-xs-1" style="width:100%;text-align:right">
+								<strong class="small">ISO Control No.: <?php echo e($header->iso_code); ?></strong>
 							</div>
 						</div>
 					</div>
